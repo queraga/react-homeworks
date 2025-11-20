665ca0dfc50c96505bb81f491822ea374641e7bd
Lashkov Yevhen <evgene@icloud.com>
2025-11-20 22:44:06 +0100

    chore: project setup and basic logic added

--- workflow2011/src/components/toggleText/index.jsx
+++ workflow2011/src/components/toggleText/index.jsx
@@ -1,18 +1,40 @@
-// import styles from "../toggleText/styles.module.css";
+import styles from "../toggleText/styles.module.css";
 import { useRef, useState } from "react";
 
 function ToggleText() {
   const textAwayRef = useRef(null);
-  const [isHidden, setIsHidden] = useState(false);
+
   const [duration, setDuration] = useState(300);
 
+  const toggleVisibility = () => {
+    if (!textAwayRef.current) return;
+    textAwayRef.current.classList.toggle(styles.hidden);
+  };
   return (
     <div>
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
         <label>
-          Vanished Text
-          <input type="text" />
+          Put here number - how long it takes to get clear
+          <br />
+          <input
+            type="number"
+            value={duration}
+            onChange={(e) => setDuration(e.target.value)}
+            placeholder="Animation duration"
+          />
         </label>
+        <button onClick={toggleVisibility}>Toggle Text</button>
+        <p
+          ref={textAwayRef}
+          className={styles.textBlock}
+          style={{ transition: `opacity ${duration}ms` }}
+        >
+          Clean the trash! Make me go away from here.
+        </p>
         <button type="submit">Get it done</button>
       </form>
     </div>
